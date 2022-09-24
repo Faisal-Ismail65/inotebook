3b3cff36c3b7ed9f8b385df2bd6e9d1f9c8446c1
FaisalIsmail <faisalkhan7842540@gmail.com>
2022-09-24 23:41:51 +0500

    adding navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,7 @@
-import './App.css';
 import Navbar from './components/Navbar';
 import Home from './components/Home';
 import About from './components/About';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
-
-
 function App() {
   return (
     <Router>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,7 +2,7 @@ import React from 'react'
 import {Link} from 'react-router-dom';
 const Navbar = () => {
   return (
-    <nav className="navbar navbar-expand-lg bg-light">
+    <nav className="navbar navbar-expand-lg bg-dark">
   <div className="container-fluid">
     <Link className="navbar-brand" to="/">INoteBook</Link>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -11,7 +11,7 @@ const Navbar = () => {
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
       <ul className="navbar-nav me-auto mb-2 mb-lg-0">
         <li className="nav-item">
-          <Link className="nav-link active" to="/home">Home</Link>
+          <Link className="nav-link active" to="/">Home</Link>
         </li>
         <li className="nav-item">
           <Link className="nav-link active" to="/about">About</Link>
