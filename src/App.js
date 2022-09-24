b82ff4d1df5dd054dbbb57eb2a5a49c8dc077601
FaisalIsmail <faisalkhan7842540@gmail.com>
2022-09-24 11:53:16 +0500

    setting router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import './App.css';
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import About from './components/About';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+
 
 function App() {
   return (
-    <div className="App">
+    <Router>
       <h1>INoteBook</h1>
-    </div>
+      <Navbar/>
+      <Routes>
+      <Route path='/home' element={<Home/>} />
+      <Route path='/about' element={<About/>}/>
+      </Routes>
+    </Router>
   );
 }
 
