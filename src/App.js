f7f844ffe80b393a56b7d121a7f8d03dd025df64
FaisalIsmail <faisalkhan7842540@gmail.com>
2022-09-25 22:14:16 +0500

    adding form to add note

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import Navbar from './components/Navbar';
 import Home from './components/Home';
 import About from './components/About';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
-import NoteState from './context/notes/noteState';
+import NoteState from './context/notes/NoteState';
 function App() {
   return (
     <NoteState>
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,21 @@
 import React from 'react'
 
 const Home = () => {
-  return (
-    <h1 className='text-center'>INoteBook</h1>
+  return (<>
+    <h1 className='text-center my-3'>Add a Note</h1>
+    <div className="mb-3">
+      <label htmlFor="title" className="form-label">Title</label>
+      <input type="text" className="form-control" id="title" />
+    </div>
+    <div className="mb-3">
+      <label htmlFor="title" className="form-label">Title</label>
+      <input type="text" className="form-control" id="title" />
+    </div>
+    <div className="mb-3">
+      <label htmlFor="description" className="form-label">Description</label>
+      <textarea className="form-control" id="description" rows="2"></textarea>
+    </div>
+  </>
   )
 }
 export default Home
