6514d3537266cbb56a5f32cb5341d2deb20a9359
FaisalIsmail <faisalkhan7842540@gmail.com>
2022-09-27 23:22:01 +0500

    adding noteitem

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,8 @@
-import React, {useContext} from 'react'
-import noteContext from '../context/notes/NoteContext'; 
+import React from 'react'
+import Notes from './Notes'
+ 
 const Home = () => {
-const context = useContext(noteContext);
-const {notes, setNotes} =  context;
+
   return (
   <>
     <h1 className='text-center my-3'>Add a Note</h1>
@@ -18,12 +18,7 @@ const {notes, setNotes} =  context;
       <label htmlFor="description" className="form-label">Description</label>
       <textarea className="form-control" id="description" rows="2"></textarea>
     </div>
-    <div className='container my-3'>
-      <h2>Your Notes</h2>
-      {notes.map((note)=>note.title)}
-
-    </div>
-
+    <Notes/>    
   </>
   )
 }
