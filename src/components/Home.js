4973e3bb71c0009ca924505bb3dee8043c2cb05d
FaisalIsmail <faisalkhan7842540@gmail.com>
2022-09-27 12:22:37 +0500

    using noteContext in Home

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,10 @@
-import React from 'react'
-
+import React, {useContext} from 'react'
+import noteContext from '../context/notes/NoteContext'; 
 const Home = () => {
-  return (<>
+const context = useContext(noteContext);
+const {notes, setNotes} =  context;
+  return (
+  <>
     <h1 className='text-center my-3'>Add a Note</h1>
     <div className="mb-3">
       <label htmlFor="title" className="form-label">Title</label>
@@ -15,6 +18,12 @@ const Home = () => {
       <label htmlFor="description" className="form-label">Description</label>
       <textarea className="form-control" id="description" rows="2"></textarea>
     </div>
+    <div className='container my-3'>
+      <h2>Your Notes</h2>
+      {notes.map((note)=>note.title)}
+
+    </div>
+
   </>
   )
 }
--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -1,12 +1,29 @@
 import NoteContext from "./NoteContext";
-
+import {useState} from 'react';
 const NoteState = (props) => {
-    const state = {
-        name : "faisal",
-        class : "it"
-    }
+    const notesInitial = [
+            {
+              "_id": "632c46dd481860eb059196db",
+              "user": "632b6e84342171c7f1b33f40",
+              "title": "note title",
+              "description": "this is the first note",
+              "tag": "this tag for the first time",
+              "date": "2022-09-22T11:28:29.798Z",
+              "__v": 0
+            },
+            {
+              "_id": "632c46de481860eb059196dd",
+              "user": "632b6e84342171c7f1b33f40",
+              "title": "note title",
+              "description": "this is the first note",
+              "tag": "this tag for the first time",
+              "date": "2022-09-22T11:28:30.261Z",
+              "__v": 0
+            }
+          ]
+    const [notes, setNotes] = useState(notesInitial);
     return (
-        <NoteContext.Provider value={state} >
+        <NoteContext.Provider value={{notes,setNotes}} >
             {props.children}
         </NoteContext.Provider>
     )
