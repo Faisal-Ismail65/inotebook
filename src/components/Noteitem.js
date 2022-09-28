c3de7a350192afdaba283e797b436529cfb6d221
FaisalIsmail <faisalkhan7842540@gmail.com>
2022-09-28 13:29:49 +0500

    adding icons of font awesome

--- src/components/Noteitem.js
+++ src/components/Noteitem.js
@@ -6,7 +6,11 @@ const Noteitem = (props) => {
     <div className='col-md-3'>
       <div className='card my-3' >
         <div className='card-body'>
+          <div className="d-flex">
           <h5 className='card-title'>{note.title}</h5>
+          <i class="fa-regular fa-trash"></i>
+          <i class="fa-regular fa-pen-to-square"></i>
+          </div>
           <p className='card-text'>{note.description}</p> 
         </div>
 
