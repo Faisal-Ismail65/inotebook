40903622fe64027ae6b7bbdb43b57401c0dc8b4f
FaisalIsmail <faisalkhan7842540@gmail.com>
2022-10-01 12:37:14 +0500

    pause

--- src/components/Noteitem.js
+++ src/components/Noteitem.js
@@ -8,8 +8,8 @@ const Noteitem = (props) => {
         <div className='card-body'>
           <div className="d-flex">
           <h5 className='card-title'>{note.title}</h5>
-          <i class="fa-regular fa-trash"></i>
-          <i class="fa-regular fa-pen-to-square"></i>
+          <i class="fa-regular fa-trash mx-2"></i>
+          <i class="fa-regular fa-pen-to-square mx-2"></i>
           </div>
           <p className='card-text'>{note.description}</p> 
         </div>
